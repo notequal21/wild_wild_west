3b91a9ef7e152c3372932ff55d799f35b84ac079
Kirill Makhnev <kalorigen@gmail.com>
2023-10-04 20:50:47 +0400

    faq updates

--- src/modules/Faq/faqList.js
+++ src/modules/Faq/faqList.js
@@ -7,9 +7,9 @@ const faqList = [
     ],
   },
   {
-    question: 'How Can I Get "Wild Wild West" Tokens?    ',
+    question: 'How Can I Get Wild Wild West Tokens?    ',
     answer: [
-      `Getting your "Wild Wild West" Tokens is easy! While we don't sell them directly on our official website, we've got you covered with <a href="#how">step-by-step instructions</a> on how to obtain them through our preferred exchange. Just make sure you have some Ethereum (ETH) in your wallet, ready for swapping into $WWW tokens.
+      `Getting your Wild Wild West Tokens is easy! While we don't sell them directly on our official website, we've got you covered with <a href="#how">step-by-step instructions</a> on how to obtain them through our preferred exchange. Just make sure you have some Ethereum (ETH) in your wallet, ready for swapping into $WWW tokens.
       `,
     ],
   },
@@ -23,14 +23,14 @@ const faqList = [
   {
     question: 'How Can I Get Involved in the $WWW Community?',
     answer: [
-      `We'd love to have you on board! Joining our community is as easy as riding a bucking bronco. Visit our <a href="https://t.me/+e7ODvfkbD102NzE0" rel="noreferrer" target="_blank">telegram group</a> and connect with other crypto adventurers, share memes, and stay updated on the latest "Wild Wild West" Token news and events.
+      `We'd love to have you on board! Joining our community is as easy as riding a bucking bronco. Visit our <a href="https://t.me/+e7ODvfkbD102NzE0" rel="noreferrer" target="_blank">telegram group</a> and connect with other crypto adventurers, share memes, and stay updated on the latest Wild Wild West Token news and events.
       `,
     ],
   },
   {
     question: 'What’s the future development of this meme token?     ',
     answer: [
-      `Well, let us assure you that we're firmly routed in the Wild Wild West of crypto for the sheer joy and adventure it brings. We're not in the business of hasty token sales or questionable practices that plague 99% of the corners of this space. Instead, we're here to surf the unpredictable waves, foster a robust and vibrant community – because in crypto, community is king – and have a blast while doing it. Oh, and we might just have a few surprises up our sleeves, secrets we'll unveil in due course. Remember, it's not just about the tech; it's about the strength of our community. So, saddle up, join our posse, and let's ride this crypto rodeo all the way to the moon!" 🤠🚀🌕
+      `Well, let us assure you that we're firmly routed in the Wild Wild West of crypto for the sheer joy and adventure it brings. We're not in the business of hasty token sales or questionable practices that plague 99% of the corners of this space. Instead, we're here to surf the unpredictable waves, foster a robust and vibrant community – because in crypto, community is king – and have a blast while doing it. Oh, and we might just have a few surprises up our sleeves, secrets we'll unveil in due course. Remember, it's not just about the tech; it's about the strength of our community. So, saddle up, join our posse, and let's ride this crypto rodeo all the way to the moon! 🤠🚀🌕
       `,
     ],
   },
